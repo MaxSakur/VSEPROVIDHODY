aec6734e098b1a383c63ce04e938c3ff0a9ad9f2
maralov <aralov.mykhailo@gmail.com>
2021-02-17 23:05:44 +0200

    добавил табы, контент  аналитика

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,19 @@
-$(function() {
+jQuery(function ($) {
 
 	// Custom JS
 
-});
+	//	TABS START
+	$(".tabs .tabs-content").not(":first").hide();
+
+	$(".tabs-nav .js-tab-btn").on('click', function () {
+
+		const $n = $(this).data('tab');
+		$(".tabs-nav .js-tab-btn").removeClass('tabs-nav_item--active')
+		$(this).addClass('tabs-nav_item--active')
+
+		$(".tabs-content").hide().eq($n).fadeIn('slow')
+		console.log($(".tabs-content").eq($n).find('iframe').attr('data-src'))
+	})
+	//TABS END
+
+}); // fix wordpress render JQ
